6c9bdc6b77673c6d791214e0698752b7b0d75bea
Blindfold <TMainForm@gmail.com>
2019-08-23 21:51:13 +0300

    New carriege installed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1133,14 +1133,14 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 200
+#define X_BED_SIZE 220
 #define Y_BED_SIZE 220
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
+#define X_MIN_POS -14
 #define Y_MIN_POS -54 
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
+#define X_MAX_POS X_BED_SIZE + 10
 #define Y_MAX_POS Y_BED_SIZE + 3
 #define Z_MAX_POS 220
 
