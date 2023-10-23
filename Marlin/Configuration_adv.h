e417f4ff6cb6fe9c4cd18b762d551fe058dad2bf
Denis Kozlov <denis.kozlov@quantori.com>
2023-10-23 16:27:53 +0400

    fix z max feedrate

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1453,7 +1453,7 @@
 // @section lcd
 
 #if HAS_MANUAL_MOVE_MENU
-  #define MANUAL_FEEDRATE { 30*60, 30*60, 8*60, 6*60 } // (mm/min) Feedrates for manual moves along X, Y, Z, E from panel
+  #define MANUAL_FEEDRATE { 30*60, 30*60, 5*60, 6*60 } // (mm/min) Feedrates for manual moves along X, Y, Z, E from panel
   #define FINE_MANUAL_MOVE 0.025    // (mm) Smallest manual move (< 0.1mm) applying to Z on most machines
   #if IS_ULTIPANEL
     #define MANUAL_E_MOVES_RELATIVE // Display extruder move distance rather than "position"
