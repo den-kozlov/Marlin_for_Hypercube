3cd41870b5caba51ad97e5c9f0c906d1d013f141
Blindfold <TMainForm@gmail.com>
2024-04-18 20:16:15 +0400

    tuning

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1241,7 +1241,7 @@
  * Override with M92 (when enabled below)
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 200, 200, 2400, 2904 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 200, 200, 2400, 3047.21 }
 
 /**
  * Enable support for M92. Disable to save at least ~530 bytes of flash.
